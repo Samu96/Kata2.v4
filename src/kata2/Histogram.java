ed8d1d52902ad9fb9bdb22cac9a7046acae87fc6
Entrar <Entrar@APD-L13>
2017-10-06 08:45:15 +0100

    Kata2.v4

--- src/kata2/Histogram.java
+++ src/kata2/Histogram.java
@@ -3,26 +3,22 @@ package kata2;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Histogram {
-    private final int[] array;
+public class Histogram<T> {
+    private final T[] array;
 
 
-    public Histogram(int[] array) {
+    public Histogram(T[] array) {
         this.array = array;
     }
     
-    public int[] getArray() {
+    public T[] getArray() {
         return array;
     }
     
-    public Map<Integer,Integer> getHistogram(){
-        Map <Integer,Integer> datos = new HashMap<>();
-        for (int i = 0; i < array.length; i++) {
-            if(datos.containsKey(array[i])){
-                datos.put(array[i], datos.get(array[i])+1);
-            }else{
-                datos.put(array[i], 1);
-            }
+    public Map<T,Integer> getHistogram(){
+        Map <T,Integer> datos = new HashMap<>();
+        for (T i: array) {
+            datos.put(i, datos.containsKey(i) ? datos.get(i) + 1 : 1);
         }
         return datos;
     }
